86bb6f6f17696e68b72908f4eab64981351d47cb
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-24 23:04:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,27 @@ $(function () {
     var socket = io();
     var messagesList = $('#messages');
 
-    // Function to display a single message
     function displayMessage(data) {
         messagesList.append($('<li>').text(`[${data.timestamp}] ${data.msg} (IP: ${data.ip})`));
     }
 
-    // Load previous chat messages from chatlog.json
     $.getJSON('/chatlog.json', function (data) {
         data.forEach(displayMessage);
     });
 
-    // ...
+    $('form').submit(function(e) {
+        e.preventDefault();
+        if ($('#input').val()) {
+            const msg = $('#input').val();
+            const timestamp = new Date().toLocaleTimeString();
+            const ip = window.location.hostname;
+            socket.emit('chat message', { msg, timestamp, ip });
+            $('#input').val('');
+        }
+        return false;
+    });
 
-    socket.on('chat message', function (data) {
+    socket.on('chat message', function(data){
         displayMessage(data);
         window.scrollTo(0, document.body.scrollHeight);
     });
