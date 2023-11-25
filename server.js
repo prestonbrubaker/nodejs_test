67268c85eaa05ac0f2660a43e7b5adf6289e8ca3
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-24 23:01:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const fs = require('fs');
 
 // Serve static files from the current directory (including JavaScript files)
 app.use(express.static(__dirname));
@@ -16,7 +17,23 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     socket.on('chat message', (data) => {
         const { msg, timestamp, ip } = data;
-        io.emit('chat message', { msg, timestamp, ip });
+        const messageData = { msg, timestamp, ip };
+        io.emit('chat message', messageData);
+
+        // Append the message to the chatlog.json file
+        fs.readFile('chatlog.json', (err, content) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            let chatLog = JSON.parse(content);
+            chatLog.push(messageData);
+            fs.writeFile('chatlog.json', JSON.stringify(chatLog, null, 4), (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        });
     });
 });
 
