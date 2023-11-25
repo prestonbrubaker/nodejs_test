11ab454568c4b635eca4102ca1d5d9f97a8fedf5
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-24 22:18:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 $(function () {
     var socket = io();
     $('#form').submit(function(e) {
-        e.preventDefault(); // This line prevents the default form submission behavior
+        e.preventDefault();
         if ($('#input').val()) {
             socket.emit('chat message', $('#input').val());
             $('#input').val('');
         }
-        return false;
     });
     socket.on('chat message', function(msg){
         $('#messages').append($('<li>').text(msg));
