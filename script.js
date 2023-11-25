5f911580fc130634fe47357fd2ea51984c45b131
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-24 22:45:24 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function () {
+            var socket = io();
+            $('form').submit(function(e) {
+                e.preventDefault();
+                if ($('#input').val()) {
+                    socket.emit('chat message', $('#input').val());
+                    $('#input').val('');
+                }
+                return false;
+            });
+            socket.on('chat message', function(msg){
+                $('#messages').append($('<li>').text(msg));
+                window.scrollTo(0, document.body.scrollHeight);
+            });
+        });
