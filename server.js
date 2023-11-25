1eea31f2dd8bfed42c187c2f6717380e9de1fcea
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-24 21:18:09 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const io = socketIo(server);
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
+// Root route to serve the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// WebSocket connection handling
 io.on('connection', (socket) => {
     console.log('A user connected');
 
@@ -26,4 +32,3 @@ const PORT = process.env.PORT || 58541;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
-
